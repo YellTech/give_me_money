744686bb576cfe30096506cf24eaae0c98dc25c8
Yell <yelltechsolutions@outlook.com>
2024-04-15 17:45:57 -0300

    editando database.py

--- model/database.py
+++ model/database.py
@@ -13,9 +13,17 @@ class DBAccess:
             messagebox.showerror("Erro", f"Erro ao conectar com o Banco de Dados: \n{str(e)}")
         self.cad_date = datetime.now().strftime('%d-%m-%Y')
     
+    def consult(self, table, id, id_sql="id"):
+        query = f'SELECT * FROM {table} WHERE {id_sql} = ?'
+        self.cursor.execute(query, (id,))
+        return self.cursor.fetchall()
+
     def close_conection(self):
         self.cursor.close()
         self.conn.close()
+
+    def messages(self, function, table, e):
+        messagebox.showerror("Erro", f"Erro ao {function} {table}:\n{str(e)}")
 
     def create_tables(self):
         self.cursor.execute('''
@@ -32,8 +40,9 @@ class DBAccess:
                             client_id INTEGER NOT NULL,
                             initial_value REAL NOT NULL,
                             interest_rate REAL,
-                            number_installments INTEGER
-                            FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
+                            initial_date TEXT NOT NULL,
+                            number_installments INTEGER,
+                            FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE
                             )
                             ''')
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS istallments(
@@ -44,19 +53,20 @@ class DBAccess:
                             pay_date TEXT NOT NULL,
                             status TEXT NOT NULL,
                             interest_rate REAL,
-                            FOREIGN KEY (debts_id) REFERENCES debts (id) ON DELETE CASCADE
+                            FOREIGN KEY (debts_id) REFERENCES debts(id) ON DELETE CASCADE
                             )
                             ''')
         self.conn.commit()
-        self.conn.close()
+        self.close_conection()
 
-    def add_client(self, name, phone):
+    def create_client(self, name, phone):
         try:
             self.cursor.execute('INSERT INTO clients (name, phone, cad_date) VALUES (?, ?, ?)',
                                 (name, phone, self.cad_date))
+            self.conn.commit()
             return True
         except sqlite3.Error as e:
-            messagebox.showerror("Erro", f"Erro ao adicionar cliente: \n{str(e)}")
+            self.messages("cadastrar", "cliente", e)
             return False
         finally:
             self.close_conection()
@@ -65,9 +75,10 @@ class DBAccess:
         try:
             self.cursor.execute('DELETE FROM clients WHERE id = ?',
                                 (client_id,))
+            self.conn.commit()
             return True
         except sqlite3.Error as e:
-            messagebox.showerror("Erro", f"Erro ao deletar cliente: \n{str(e)}")
+            self.messages("deletar", "cliente", e)
             return False
         finally:
             self.close_conection()
@@ -76,9 +87,80 @@ class DBAccess:
         try:
             self.conn.execute('UPDATE clients SET name = ?, phone = ?, WHERE id = ?',
                               (new_name, new_phone, client_id))
+            self.conn.commit()
             return True
         except sqlite3.Error as e:
-            messagebox.showerror("Erro", f"Erro ao atulizar cliente:\n{str(e)}")
+            self.messages("atualizar", "cliente", e)
             return False
         finally:
             self.close_conection()
+    
+    def create_debts(self, client_id, initial_value, interest_rate, initial_date, number_installments):
+        try:
+            self.conn.execute('''INSERT INTO debts (
+                              client_id, initial_value, interest_rate, initial_date, number_installments)
+                              VALUES (?, ?, ?, ?, ?)''',
+                              (client_id, initial_value, interest_rate, initial_date, number_installments))
+            self.conn.commit()
+            return True
+        except sqlite3.Error as e:
+            self.messages("cadastrar", "dívida", e)
+            return False
+        finally:
+            self.close_conection()
+    
+    def delete_debts(self, debts_id):
+        try:
+            self.conn.execute('DELETE FROM debts WHERE id = ?', (debts_id))
+            self.conn.commit()
+            return True
+        except sqlite3.Error as e:
+            self.messages("deletar", "dívida", e)
+        finally:
+            self.close_conection()
+    
+    def update_debts(self, debts_id, new_initial_value,
+                     new_interest_rate, new_initial_date, new_number_installments):
+        try:
+            self.conn.execute('''UPDATE debts SET initial_value = ?,
+                              interest_rate = ?, number_installments = ?, WHERE id = ?''',
+                              (new_initial_value, new_interest_rate, new_initial_date, new_number_installments, debts_id))
+            self.conn.commit()
+            return True
+        except sqlite3.Error as e:
+            self.messages("atualizar", "dívida", e)
+            return False
+        finally:
+            self.close_conection()
+
+    def create_installments(self, debts_id, installment, installment_value, pay_date, status, interest_rate):
+        try:
+            self.conn.execute('''INSERT INTO installments (
+                              debts_id, installment, installment_value, pay_date, status, interest_rate)
+                              VALUES (?, ?, ?, ?, ?)''', 
+                              (debts_id, installment, installment_value, pay_date, status, interest_rate))
+            self.conn.commit()
+            return True
+        except sqlite3.Error as e:
+            self.messages("cadastrar", "parcela", e)
+            return False
+        finally:
+            self.close_conection()
+
+    def delete_installments(self, debts_id):
+        try:
+            self.conn.execute('DELETE FROM installments WHERE debts_id = ?', (debts_id))
+            self.conn.commit()
+            return True
+        except sqlite3.Error as e:
+            self.messages("deletar", "parcelas", e)
+            return False
+        finally:
+            self.close_conection()
+
+if __name__ == "__main__":
+    db_table = DBAccess()
+    db_table.create_tables()
+    db = DBAccess()
+    teste = db.consult("debts", 1, "id")
+    print(teste)
